79b4e760a763d0480419540898647e3585548ef5
Yang Chuanzhang <json@riemann.local>
2013-10-20 09:55:52 +0100

    Add toString methods to Grammar classes and a few TODOs

--- js_compiler.scala
+++ js_compiler.scala
@@ -12,26 +12,39 @@ val fresh = (() =>  {
    }
 })()
 
-case class SVar(x: Idn)
+case class SVar(x: Idn) { override def toString = x }
 
 abstract class SCon
-case class SInt(i: Int) extends SCon
-case class SDouble(d: Double) extends SCon
-case class SBool(b: Boolean) extends SCon
+case class SInt(i: Int) extends SCon { override def toString = i.toString() }
+// TODO double
+case class SBool(b: Boolean) extends SCon { override def toString =  if (b) "#t" else "#f" }
+// TODO symbols
 
 abstract class SPrim
-case class SPrimPlus extends SPrim
-case class SPrimMinus extends SPrim
-case class SPrimMult extends SPrim
+case class SPrimPlus() extends SPrim { override def toString = "+" }
+case class SPrimMinus() extends SPrim { override def toString = "-" }
+case class SPrimMult() extends SPrim { override def toString = "*" }
+// TODO boolean operators
 
 abstract class SForm
 abstract class SExp extends SForm
-case class SVarExp(x: SVar) extends SExp
-case class SConExp(i: SCon) extends SExp
-case class SIfExp(t: SExp, e1: SExp, e2: SExp) extends SExp
-case class SProcExp(vs: List[SVar], e: SExp) extends SExp
-case class SPrimExp(p: SPrim, e1: SExp, e2: SExp) extends SExp
-case class SAppExp(e1: SExp, e2: SExp) extends SExp
+case class SVarExp(x: SVar) extends SExp { override def toString = x.toString() }
+case class SConExp(i: SCon) extends SExp { override def toString = i.toString() }
+case class SIfExp(t: SExp, e1: SExp, e2: SExp) extends SExp { override def toString = "(if " + e1.toString() + " " + e2.toString() + ")" }
+case class SLambdaExp(v: SVar, e: SExp) extends SExp { override def toString = "(lambda (" + v.toString() + ") " + e.toString() + ")" }
+// TODO multiple variables
+// TODO define
+// TODO begin
+case class SPrimExp(p: SPrim, e1: SExp, e2: SExp) extends SExp { override def toString = "(" + p.toString() + " " + e1.toString() + " " +
+  e2.toString() + ")" }
+// TODO more than two expressions
+case class SAppExp(e1: SExp, e2: SExp) extends SExp { override def toString = "(" + e1.toString() + " " + e2.toString() + ")"}
 
 
 
+
+
+
+val prog0 = SPrimExp(SPrimPlus(), SConExp(SInt(1)), SConExp(SInt(2)))
+
+println(prog0.toString())
