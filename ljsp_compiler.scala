edc1a1d0ebbe589431e7c2176633f7d58a3a651a
Jan Soendermann <jan.soendermann@googlemail.com>
2013-10-23 16:50:42 +0100

    Implement define, implement CPS translation for all expressions except let

--- ljsp_compiler.scala
+++ ljsp_compiler.scala
@@ -13,8 +13,6 @@ type Idn = String
 
 
 
-// TODO define
-// TODO set!
 abstract class SExp
 case class SIdn(idn: Idn) extends SExp { override def toString = idn }
 case class SInt(i: Int) extends SExp { override def toString = i.toString() }
@@ -22,7 +20,9 @@ case class SBool(b: Boolean) extends SExp { override def toString = if (b) "#t" 
 // TODO double
 // TODO symbols
 case class SIf(e1: SExp, e2: SExp, e3: SExp) extends SExp { override def toString = "(if " + e1.toString() + " " + e2.toString() + " " + e3.toString() + ")" }
-case class SLambda(idns: List[SIdn], e: SExp) extends SExp { override def toString = "(lambda (" + idns.mkString(" ") + ") " + e.toString() + ")" }
+case class SLambda(params: List[SIdn], e: SExp) extends SExp { override def toString = "(lambda (" + params.mkString(" ") + ") " + e.toString() + ")" }
+case class SDefine(name: SIdn, params: List[SIdn], e: SExp) extends SExp { override def toString = "(define (" + name.toString() + " " +
+  params.mkString(" ") + ") " + e.toString() + ")" }
 // TODO begin
 case class SAppl(proc: SExp, es: List[SExp]) extends SExp { override def toString = "(" + proc.toString() + " " + es.mkString(" ") + ")"}
 case class SLet(idn: SIdn, e1: SExp, e2: SExp) extends SExp { override def toString = "(let ((" + idn.toString() + " " + e1.toString() + ")) " + e2.toString() + ")" }
@@ -36,9 +36,18 @@ val fresh = (() =>  {
    var counter = -1
    () => {
       counter +=1
-      SIdn("nv" ++ "_" ++ counter.toString())
+      SIdn("new_var" ++ "_" ++ counter.toString())
    }
 })()
+
+val fresh_continuation_function = (() =>  {
+   var counter = -1
+   () => {
+      counter +=1
+      SIdn("cont" ++ "_" ++ counter.toString())
+   }
+})()
+
 
 
 
@@ -59,17 +68,21 @@ object JLispParsers extends JavaTokenParsers {
   }
     
   def lambda: Parser[SLambda] = "("~>"lambda"~>"("~>rep1(identifier, identifier)~")"~expression<~")" ^^ {
-    case idns~")"~e => SLambda(idns, e)
+    case params~")"~e => SLambda(params, e)
+  }
+
+  def define: Parser[SDefine] = "("~>"define"~>"("~>identifier~rep(identifier)~")"~expression<~")" ^^ {
+    case name~params~")"~e => SDefine(name, params, e)
   }
   def application: Parser[SAppl] = "("~>expression~rep1(expression, expression)<~")" ^^ {
     case proc~es => SAppl(proc, es)
   }
   def let: Parser[SLet] = "("~>"let"~>"("~"("~>identifier~expression~")"~")"~expression<~")" ^^ {
-    case idn~e1~")"~")"~e2 => SLet(idn, e1, e1)
+    case idn~e1~")"~")"~e2 => SLet(idn, e1, e2)
   }
   def halt: Parser[SHalt] = "("~>"halt"~>expression<~")" ^^ (e => SHalt(e))
 
-  def expression: Parser[SExp] = identifier | integer | boolean | _if | lambda | application | let | halt
+  def expression: Parser[SExp] = identifier | integer | boolean | _if | lambda | define| application | let | halt
 
   // TODO this throws an exception instead of printing a nice error message when the input isn't well formed
   def parseExpr(str: String) = parse(expression, str).get
@@ -93,11 +106,15 @@ def CPS(e: SExp, k: SExp => SExp) : SExp = e match {
         SIf(ce1, CPS(e2, k), CPS(e3, k)))
   }
 
-  // TODO lambda
-  /*case SLambda(idns, e) => {
-    val f = fresh()
-    k(SLambda(idns ::: List(f), CPSTail(e, f)))
-  }*/
+  case SLambda(params, e) => {
+    val c = fresh_continuation_function()
+    k(SLambda(params ::: List(c), CPSTail(e, c)))
+  }
+
+  case SDefine(name, params, e) => {
+    val c = fresh_continuation_function()
+    k(SDefine(name, params ::: List(c), CPSTail(e, c)))
+  }
 
   // For applications, evaluate proc first, then all the arguments
   case SAppl(proc, es) => {
@@ -111,14 +128,19 @@ def CPS(e: SExp, k: SExp => SExp) : SExp = e match {
       // this function calls CPS recursively for all expressions in es and accumulates the result in ces
       // the last call with an empty es creates a let and calls k on the result of the application
       def aux(es: List[SExp], ces: List[SExp]) : SExp = es match {
-        case Nil => SLet(f, SAppl(cproc, ces), k(f))
+        case Nil => SAppl(cproc, ces ::: List(SLambda(List(f), k(f)))) //SLet(f, SAppl(cproc, ces), k(f))
         case (e::es) => CPS(e, (ce: SExp) => aux(es, ces ::: List(ce)))
       }
-    aux(es, Nil)
+      aux(es, Nil)
     })
   }
 
-  // TODO let
+  // TODO is this correct?
+  /*case SLet(idn, e1, e2) => {
+    CPS(e1, (ce1: SExp) =>
+        CPS(e2, (ce2: SExp) =>
+            SLet(idn, ce1, k(ce2))))
+  }*/
 
   // For halt, evaluate the expression to halt with
   // TODO maybe there is a better way to do this?
@@ -129,7 +151,7 @@ def CPS(e: SExp, k: SExp => SExp) : SExp = e match {
   }
 }
 
-/*def CPSTail(e: SExp, c: SExp) : SExp = e match {
+def CPSTail(e: SExp, c: SExp) : SExp = e match {
   // For atomic values, no CPS-Translation is necessary. Simply apply k to e
   case SIdn(idn) => SAppl(c, List(e))
   case SInt(i) => SAppl(c, List(e))
@@ -141,11 +163,16 @@ def CPS(e: SExp, k: SExp => SExp) : SExp = e match {
         SIf(ce1, CPSTail(e2, c), CPSTail(e3, c)))
   }
 
-  // TODO lambda
-  //case SLambda(idns, e) => {
-  //  val f = fresh()
-  //  k(SLambda(idns ::: List(f), CPSTail(e, f)))
-  //}
+  case SLambda(params, e) => {
+    val c_ = fresh_continuation_function()
+    SAppl(c, List((SLambda(params ::: List(c_), CPSTail(e, c_)))))
+  }
+  
+  case SDefine(name, params, e) => {
+    val c_ = fresh_continuation_function()
+    SAppl(c, List((SDefine(name, params ::: List(c_), CPSTail(e, c_)))))
+  }
+
 
   // For applications, evaluate proc first, then all the arguments
   case SAppl(proc, es) => {
@@ -159,14 +186,20 @@ def CPS(e: SExp, k: SExp => SExp) : SExp = e match {
       // this function calls CPS recursively for all expressions in es and accumulates the result in ces
       // the last call with an empty es creates a let and calls k on the result of the application
       def aux(es: List[SExp], ces: List[SExp]) : SExp = es match {
-        case Nil => SLet(f, SAppl(cproc, ces), SAppl(c, List(f)))
+        case Nil => SAppl(cproc, ces ::: List(SLambda(List(f), SAppl(c, List(f))))) //SLet(f, SAppl(cproc, ces), k(f))
+        //case Nil => SLet(f, SAppl(cproc, ces), SAppl(c, List(f)))
         case (e::es) => CPS(e, (ce: SExp) => aux(es, ces ::: List(ce)))
       }
     aux(es, Nil)
     })
   }
 
-  // TODO let
+  // TODO is this correct?
+  /*case SLet(idn, e1, e2) => {
+    CPS(e1, (ce1: SExp) =>
+        CPS(e2, (ce2: SExp) =>
+            SLet(idn, ce1, SAppl(c, List(ce2)))))
+  }*/
 
   // For halt, evaluate the expression to halt with
   // TODO maybe there is a better way to do this?
@@ -175,13 +208,14 @@ def CPS(e: SExp, k: SExp => SExp) : SExp = e match {
     CPS(e, (x: SExp) =>
         SLet(f, x, SAppl(c, List(f))))
   }
-}*/
+}
 
 
 
 // ################ Simple Tests ####################
 
-val prog3 = JLispParsers.parseExpr("(if #t 1 2)")
+//val prog3 = JLispParsers.parseExpr("(let ((z 1)) (+ z 3)))")
+val prog3 = JLispParsers.parseExpr("((lambda (z) (+ z 3)) 1)")
 val prog3cps = CPS(prog3, (x: SExp) => SHalt(x))
 
 println(prog3.toString())
