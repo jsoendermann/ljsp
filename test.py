21c23fa63269f5865ff422e26cab41a28fa5f8af
Jan Soendermann <jan.soendermann@googlemail.com>
2013-10-28 15:51:08 +0000

    Improve TODO description in comment

---
+++ test.py
@@ -0,0 +1,42 @@
+#!/usr/local/bin/python
+
+import subprocess
+import glob
+import os
+
+### COLORS ###
+
+GREEN = '\033[92m'
+RED = '\033[91m'
+OFF = '\033[0m'
+
+RACKET_PATH = "/Applications/Racket v5.3.6/bin/racket"
+
+
+def scheme_eval(source):
+	return call_prog([RACKET_PATH, "--eval", source])
+
+# call a program and return its output
+def call_prog(prog_and_args):
+	proc = subprocess.Popen(prog_and_args, stdout=subprocess.PIPE)
+	(output, err) = proc.communicate()
+	return output.strip()
+
+
+for source_file_path in glob.glob("./tests/*.ljsp"):
+	with open(source_file_path, "r") as source_file:
+		print "Testing "+os.path.basename(source_file_path)
+		source = source_file.read()
+		print " Compiling to CPS..."
+		cps = call_prog(["scala", "ljsp_compiler.scala", "--cps", source])
+		# TODO this shouldn't succeed when both evaluations fail with the same error
+		print " Evaluating source..."
+		res_source = scheme_eval(source)
+		print " Evaluating CPS..."
+		res_cps = scheme_eval(cps)
+		if res_source == res_cps:
+			print GREEN + " Success" + OFF
+		else:
+			print RED + " Fail!" + OFF
+		print ""
+
