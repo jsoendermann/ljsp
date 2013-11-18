6a520a8158ce8477d59e2b721fb253f4d34678ea
Jan Soendermann <jan.soendermann@googlemail.com>
2013-11-18 00:00:52 +0000

    Comment and indent test_lib

--- test_lib.scm
+++ test_lib.scm
@@ -1,9 +1,22 @@
+; This file contains functions that are used in intermediate stages
+; in the compiler to test those stages.
+
+
+
+; This function takes a list of variable length and evaluates all
+; elements of the list that are not lists, procedures and integers
+; as evaluating those is invalid scheme
+; TODO There should be a better way to do this
 (define (make-env . args) 
-  (map (lambda (x) (if (or (list? x) (procedure? x) (integer? x)) x (eval x))) args))
+  (map (lambda (x) (if (or (list? x) 
+                           (procedure? x) 
+                           (integer? x)) 
+                     x 
+                     (eval x))) args))
 
 (define (nth n l) (if (= n 0)
-  (car l)
-  (nth (- n 1) (cdr l))))
+                    (car l)
+                    (nth (- n 1) (cdr l))))
 
 (define (make-lambda l e) `(,l ,e))
 
@@ -11,4 +24,8 @@
 
 (define (get-env l*) (nth 1 l*))
 
-(define (hoisted-lambda f e) (lambda args (apply f (cons e args))))
+; This function takes a function f and an argument e and returns a lambda
+; that, when called, calls function f with a list consisting of e and all
+; additional arguments the lambda is called with
+(define (hoisted-lambda f e) (lambda args 
+                               (apply f (cons e args))))
