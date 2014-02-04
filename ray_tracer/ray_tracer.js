67c55327c7c578d9f0c83f75701e3f3f35e44920
Jan Soendermann <jan.soendermann@googlemail.com>
2014-02-04 22:38:57 +0000

    Different way to compute eye rays

--- ray_tracer/ray_tracer.js
+++ ray_tracer/ray_tracer.js
@@ -1,19 +1,29 @@
-(function () {
-    "use strict";
+//(function () {
+//    "use strict";
+//
+//    window.onload = function () { 
+//      var result = document.getElementById("result"),
+//            canvas = result.getContext("2d"),
+//            width = result.width,
+//            height = result.height,
+//            imageData = canvas.createImageData(width, height),
+//            spheres,
+//            light;
 
-    window.onload = function () { 
-        var result = document.getElementById("result"),
-            canvas = result.getContext("2d"),
-            width = result.width,
-            height = result.height,
-            imageData = canvas.createImageData(width, height),
+        var result,
+            canvas,
+            width,
+            height,
+            imageData,
             spheres,
             light;
 
 
-        function Sphere(pos, r) {
+
+        function Sphere(pos, r, clr) {
             this.pos = pos;
             this.r = r;
+            this.clr = clr;
         }
 
         function Light(pos) {
@@ -37,6 +47,19 @@
             };
         }
         
+        function Colour(r, g, b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        function scaled_colour(s, c) {
+            return new Colour(min(max(c.r*s, 0), 255), min(max(c.g*s, 0), 255), min(max(c.b*s, 0), 255));
+        }
+
+        function add_colours_with_weights(w1, c1, w2, c2) {
+            return new Colour(w1*c1.r + w2*c2.r, w1*c1.g + w2*c2.g, w1*c1.b + w2*c2.b);
+        }
 
         function Ray(org, dir) {
             this.org = org;
@@ -54,6 +77,18 @@
             return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
         }
 
+        function cross_product(u, v) {
+            return new Vector3(u.y*v.z - u.z*v.y, u.z*v.x - u.x*v.z, u.x*v.y - u.y*v.x);
+        }
+        
+        function scala_vector_product(s, v) {
+            return new Vector3(v.x * s, v.y * s, v.z * s);
+        }
+
+        function add_vectors(v1, v2) {
+            return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
         function difference_vector(v1, v2) {
             return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
@@ -66,7 +101,7 @@
             return (x<y?x:y);
         }
         
-        function closestIntersectionPoint(r_org, s) {
+        function intersectionPoint(r_org, s) {
             var r, A, B, C, discriminant, t0, t1;
 
             // r_org in object space of s
@@ -100,35 +135,71 @@
             return t0;
         }
 
-
-        function trace(r) {
-            var i, sphere, smallestK, k, intersectionPoint, normal, dirToLight, intensity;
-
-            smallestK = Infinity;
-
+        
+        // returns [k, point, sphere]
+        function closestIntersectionPoint(r) {
+            var smallestK = Infinity, closestSphere = null;
+            
             for (i = 0; i < spheres.length; i++) {
                 sphere = spheres[i];
-                k = closestIntersectionPoint(r, spheres[i]);
-                smallestK = min(smallestK, k);
+                k = intersectionPoint(r, sphere);
+                if (k < smallestK) {
+                    smallestK = k;
+                    closestSphere = sphere;
+                }
             }
-            if (smallestK === Infinity) {
-                return [60, 60, 60];
+
+            return [smallestK, add_vectors(r.org, scala_vector_product(smallestK, r.dir)), closestSphere];
+        }
+
+
+
+        function trace(r, recursion_depth) {
+            var i, sphere, closestSphere, k, intersectionPoint, normal, dirToLight, intensity, reflectionVector, reflectedRay, reflectedColour;
+
+            closest = closestIntersectionPoint(r);
+            k = closest[0];
+            intersectionPoint = closest[1];
+            closestSphere = closest[2];
+
+            //console.log(closest);
+
+            if (k === Infinity) {
+                return new Colour(0, 0, 0); //Colour(60, 60, 60);
             } else {
-                intersectionPoint = new Vector3(r.org.x + smallestK * r.dir.x, r.org.y + smallestK * r.dir.y, r.org.z + smallestK * r.dir.z);
-                normal = difference_vector(intersectionPoint, sphere.pos);
+                intersectionPoint = new Vector3(r.org.x + k * r.dir.x, r.org.y + k * r.dir.y, r.org.z + k * r.dir.z);
+                normal = difference_vector(intersectionPoint, closestSphere.pos);
                 normal.normalise();
 
                 dirToLight = difference_vector(light.pos, intersectionPoint);
                 dirToLight.normalise();
 
                 intensity = dot_product(normal, dirToLight);
-                
-                return [max(0, 255*intensity), max(0, 200*intensity), max(0, 255*intensity)];
+
+                //if (recursion_depth > 0) {
+                //    reflectionVector = difference_vector(r.dir, scala_vector_product(2 * dot_product(r.dir, normal), normal));
+                //    reflectionVector.normalise();
+
+                //    reflectedRay = new Ray(intersectionPoint, reflectionVector);
+
+                //    reflectedColour = trace(reflectedRay, recursion_depth - 1);
+    
+                //    //console.log(reflectedColour);
+                //    if (reflectedColour.r != 0 || reflectedColour.g != 0 || reflectedColour.b != 0)
+                //        return add_colours_with_weights(0.5, reflectedColour, 0.5, new Colour(255, 200, 255));
+                //    else
+                //        return new Colour(255, 200, 255);
+
+
+
+                //} else {
+                    return scaled_colour(intensity, closestSphere.clr);
+                //}
             }
             
         }
 
-        function setPixelToColor(_imageData, x, y, r, g, b) {
+        function setPixelToRGB(_imageData, x, y, r, g, b) {
             var i = (x + y * _imageData.width) * 4;
             _imageData.data[i] = r;
             _imageData.data[i+1] = g;
@@ -136,30 +207,51 @@
             _imageData.data[i+3] = 255;
         }
 
-        function setPixelToArray(_imageData, x, y, a) {
-            setPixelToColor(_imageData, x, y, a[0], a[1], a[2]);
+
+        function setPixelToColour(_imageData, x, y, c) {
+            setPixelToRGB(_imageData, x, y, c.r, c.g, c.b);
         }
 
 
-        spheres = [new Sphere(new Vector3(-120, 20, -1000), 110),
-                new Sphere(new Vector3(100, -40, -1000), 90)];
-        light = new Light(new Vector3(0, 800, 0));
+        spheres = [new Sphere(new Vector3(0, 0, 300), 20, new Colour(255, 200, 255)),
+                new Sphere(new Vector3(100, -40, 300), 30, new Colour(200, 255, 255)),
+                new Sphere(new Vector3(-100, 30, 300), 25, new Colour(255, 255, 200))];
+        light = new Light(new Vector3(0, 400, 0));
 
-        var field_of_view = 30;
-        var a = Math.tan((field_of_view/2.0) * (Math.PI/180.0));
-        var y, x;
-        var r;
+        window.onload = function () { 
+        result = document.getElementById("result");
+        canvas = result.getContext("2d");
+        width = result.width;
+        height = result.height;
+        imageData = canvas.createImageData(width, height);
+
+        var x, y, r;
+        var cameraDirection, cameraUp, cameraRight;
+        var normalised_x, normalised_y;
+        var t1, t2, t3, ray_dir;
+
+        cameraDirection = new Vector3(0, 0, 1);
+        cameraUp = new Vector3(0, 1, 0);
+        cameraRight = cross_product(cameraDirection, cameraUp);
+
         for (y = 0; y < height; y++) {
             for (x = 0; x < width; x++) {
-                r = new Ray(new Vector3(0, 0, 0), new Vector3(
-                            ((x / width) * 2 - 1) * a * 1, 
-                            (((y / height) * 2 - 1) * (-1)) * a, 
-                            -1));
+                normalised_x = (x/width)-0.5;
+                normalised_y = (y/height)-0.5;
 
-                setPixelToArray(imageData, x, y, trace(r));
+                t1 = scala_vector_product(normalised_x, cameraRight);
+                t2 = scala_vector_product(normalised_y, cameraUp);
+                t3 = add_vectors(t1, t2);
+                ray_dir = add_vectors(t3, cameraDirection);
+
+                r = new Ray(new Vector3(0, 0, 0), ray_dir);
+
+
+                setPixelToColour(imageData, x, y, trace(r, 1));
             }
         }
 
         canvas.putImageData(imageData, 0, 0);
-    };
-}());
+        }
+//    };
+//}());
