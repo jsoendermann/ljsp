069d55194c4032f975647a1450ea1af1ab6ea6ff
Jan Soendermann <jan.soendermann@googlemail.com>
2014-02-08 23:07:23 +0000

    Refactor raytracing lisp code to use multi variable let

--- ray_tracer/ljsp_code.scm
+++ ray_tracer/ljsp_code.scm
@@ -6,25 +6,25 @@
 ; intersect the sphere, the function returns 1,000,000.0
 (define (raySphereIntersectionPoint ro_org_x ro_org_y ro_org_z ro_dir_x ro_dir_y ro_dir_z s_x s_y s_z s_r)
   ; Shift ro_org to the object space of the sphere
-  (let ((r_org_x (- ro_org_x s_x)))
-    (let ((r_org_y (- ro_org_y s_y)))
-      (let ((r_org_z (- ro_org_z s_z)))
-        
-        (let ((A (vectorsDotProduct ro_dir_x ro_dir_y ro_dir_z ro_dir_x ro_dir_y ro_dir_z)))
-          (let ((B (* 2 (vectorsDotProduct r_org_x r_org_y r_org_z ro_dir_x ro_dir_y ro_dir_z))))
-            (let ((C (- (vectorsDotProduct r_org_x r_org_y r_org_z r_org_x r_org_y r_org_z) (* s_r s_r))))
-              (let ((disc (- (* B B) (* 4 A C))))
-                ; If the discriminant is negative, the equation has no solution and the ray does not intersect
-                ; the sphere
-                (if (< disc 0.0)
-                  1000000.0
-                  (let ((t0 (/ (- (neg B) (sqrt disc)) (* 2.0 A))))
-                    (let ((t1 (/ (+ (neg B) (sqrt disc)) (* 2.0 A))))
-                      (let ((t_max (max t0 t1)))
-                        (let ((t_min (min t0 t1)))
-                          (if (< t_max 0.0)
-                            1000000.0
-                            (if (< t_min 0)
-                              t_max
-                              t_min)))))))))))))))
+  (let ((r_org_x (- ro_org_x s_x))
+        (r_org_y (- ro_org_y s_y))
+        (r_org_z (- ro_org_z s_z)))
+
+    (let ((A (vectorsDotProduct ro_dir_x ro_dir_y ro_dir_z ro_dir_x ro_dir_y ro_dir_z))
+          (B (* 2 (vectorsDotProduct r_org_x r_org_y r_org_z ro_dir_x ro_dir_y ro_dir_z)))
+          (C (- (vectorsDotProduct r_org_x r_org_y r_org_z r_org_x r_org_y r_org_z) (* s_r s_r))))
+      (let ((disc (- (* B B) (* 4 A C))))
+        ; If the discriminant is negative, the equation has no solution and the ray does not intersect
+        ; the sphere
+        (if (< disc 0.0)
+          1000000.0
+          (let ((t0 (/ (- (neg B) (sqrt disc)) (* 2.0 A)))
+                (t1 (/ (+ (neg B) (sqrt disc)) (* 2.0 A))))
+            (let ((t_max (max t0 t1))
+                  (t_min (min t0 t1)))
+              (if (< t_max 0.0)
+                1000000.0
+                (if (< t_min 0)
+                  t_max
+                  t_min)))))))))))))))
 
