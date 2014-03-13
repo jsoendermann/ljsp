75de59f6bf0369bb2677c2890f87bc9af674dd66
Jan Soendermann <jan.soendermann@googlemail.com>
2014-03-13 16:21:19 +0000

    Fix bugs in expand LetNs stage that would cause letNs not to get converted to lets

--- 1_expand_let_ns.scala
+++ 1_expand_let_ns.scala
@@ -15,10 +15,10 @@ object expand_let_ns {
     case SIf(e1, e2, e3) => SIf(expand_let_ns(e1), expand_let_ns(e2), expand_let_ns(e3))
     case SLambda(params, e) => SLambda(params, expand_let_ns(e))
     case SDefine(name, params, e) => SDefine(name, params, expand_let_ns(e))
-    case SAppl(proc, es) => SAppl(proc, es.map{expand_let_ns})
+    case SAppl(proc, es) => SAppl(expand_let_ns(proc), es.map{expand_let_ns})
     case SApplPrimitive(proc, es) => SApplPrimitive(proc, es.map{expand_let_ns})
     case SLetN(idnsAndE1s, e2) => {
-      idnsAndE1s.foldRight(expand_let_ns(e2)){case (idnAndE1, _e2) => SLet(idnAndE1.idn, idnAndE1.e, _e2)}
+      idnsAndE1s.foldRight(expand_let_ns(e2)){case (idnAndE1, _e2) => SLet(idnAndE1.idn, expand_let_ns(idnAndE1.e), _e2)}
     }
   }
 }
