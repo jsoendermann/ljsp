f586966bb2b0b5076af915dfbbb20a905f6e2d47
Jan Soendermann <jan.soendermann@googlemail.com>
2014-03-19 11:26:33 +0000

    Fix bug that would throw an exception when compiling if expressions with complex conditions

--- 2_remove_negs.scala
+++ 2_remove_negs.scala
@@ -15,7 +15,7 @@ object remove_negs {
 
     case SIf(e1, e2, e3) => {
       e1 match {
-        // TODO This only works if the primitive operation ("not, "and", "or") is the first expression
+        // TODO This only works if the primitive operation ("not, "and", "or", etc.) is the first expression
         //      of an if expression, not if the result of a boolean expression is assigned to a variable.
         //      The code in the SApplPrimitive case below throws an exception if the user tries to do that.
 
@@ -30,26 +30,20 @@ object remove_negs {
         }
 
         // reduce (if (not b) t f) to (if b f t)
-        case SApplPrimitive(SIdn("not"), es) => SIf(remove_negs(es(0).asInstanceOf[SApplPrimitive]), remove_negs(e3), remove_negs(e2))
+        case SApplPrimitive(SIdn("not"), es) => remove_negs(SIf(es(0).asInstanceOf[SApplPrimitive], e3, e2))
         // reduce (if (and b1 b2) t f) to (if b1 (if b2 t f) f)
         case SApplPrimitive(SIdn("and"), es) => {
-          val b1 = remove_negs(es(0).asInstanceOf[SApplPrimitive])
-          val b2 = remove_negs(es(1).asInstanceOf[SApplPrimitive])
+          val b1 = es(0).asInstanceOf[SApplPrimitive]
+          val b2 = es(1).asInstanceOf[SApplPrimitive]
 
-          val true_branch = remove_negs(e2)
-          val false_branch = remove_negs(e3)
-          
-          SIf(b1, SIf(b2, true_branch, false_branch), false_branch)
+          remove_negs(SIf(b1, SIf(b2, e2, e3), e3))
         }
         // reduce (if (or b1 b2) t f) to (if b1 t (if b2 t f))
         case SApplPrimitive(SIdn("or"), es) => {
-          val b1 = remove_negs(es(0).asInstanceOf[SApplPrimitive])
-          val b2 = remove_negs(es(1).asInstanceOf[SApplPrimitive])
+          val b1 = es(0).asInstanceOf[SApplPrimitive]
+          val b2 = es(1).asInstanceOf[SApplPrimitive]
 
-          val true_branch = remove_negs(e2)
-          val false_branch = remove_negs(e3)
-          
-          SIf(b1, true_branch, SIf(b2, true_branch, false_branch))
+          remove_negs(SIf(b1, e2, SIf(b2, e2, e3)))
         }
         
         case _ => SIf(remove_negs(e1), remove_negs(e2), remove_negs(e3))
