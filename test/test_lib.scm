8e46b5d633c080e39f8fedb44fed2df992bc9340
Jan Soendermann <jan.soendermann@googlemail.com>
2014-04-04 12:39:37 +0100

    Tidy up and comment code in test_lib.scm

--- test/test_lib.scm
+++ test/test_lib.scm
@@ -2,11 +2,14 @@
 ; in the compiler. They are used by run_tests.py to test those stages.
 
 
+; The negation operator
+(define (neg x) (- x))
 
 ; This function takes a list of variable length and evaluates all
 ; elements of the list that are not lists, procedures and integers
-; as evaluating those is invalid scheme
-; TODO There should be a better way to do this
+; as evaluating those produces an error. Identifiers are evaluated,
+; however, as their value should be saved in the environment
+; TODO There might be a cleaner way to do this.
 (define (make-env . args) 
   (map (lambda (x) (if (or (list? x) 
                            (procedure? x) 
@@ -14,20 +17,24 @@
                      x 
                      (eval x))) args))
 
+; This function takes two expressions and retuns a two-element list
+; that consists of whatever the two expressions evaluate to.
+(define (make-closure l e) `(,l ,e))
+
+; This function takes an integer n and a list l and returns the
+; nth element in l.
 (define (nth n l) (if (= n 0)
                     (car l)
                     (nth (- n 1) (cdr l))))
 
-(define (make-closure l e) `(,l ,e))
+; This two functiosn take a closure and return proc and env of the
+; closure respectively. Because closures are implemented as lists,
+; this means returning the 0th and 1st elements.
+(define (get-proc l) (nth 0 l))
+(define (get-env l) (nth 1 l))
 
-(define (get-proc l*) (nth 0 l*))
-
-(define (get-env l*) (nth 1 l*))
-
-; This function takes a function f and an argument e and returns a lambda
-; that, when called, calls function f with a list consisting of e and all
+; This function takes a function f and an argument env and returns a lambda
+; that, when called, calls function f with a list consisting of env and all
 ; additional arguments the lambda is called with
-(define (hoisted-lambda f e) (lambda args 
-                               (apply f (cons e args))))
-
-(define (neg x) (- x))
+(define (hoisted-lambda f env) (lambda args 
+                                 (apply f (cons env args))))
